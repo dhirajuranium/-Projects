335e9755859e8b7ec227840e49b34750abd61138
Sourabh kumar <sourabhkumar811307@gmail.com>
2022-06-30 19:13:38 +0530

    add status code in wrong url

--- src/route/route.js
+++ src/route/route.js
@@ -15,7 +15,7 @@ router.get('/functionup/collegeDetails',collegeController.getCollegeDetail);
 
 //BAD URL VALIDATION
 router.all("*" , (req,res)=>{
-    res.status(400).send({ msg:"NOT FOUND THIS URL"})
+    res.status(404).send({ msg:"NOT FOUND THIS URL"})
 })
 
 module.exports = router;
