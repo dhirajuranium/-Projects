040474cc5c7b24290b5f17feb09c1509754cfb39
dhirajuranium <dhirajpatil2311@gmail.com>
2022-07-24 11:30:58 +0530

    done

--- src/route/route.js
+++ src/route/route.js
@@ -9,6 +9,7 @@ router.post('/url/shorten', urlController.createShortenUrl)
 
 router.get('/:urlCode', urlController.getUrl)
 
+
 //================= BAD URL VALIDATION ============================================
 router.all("*", (req, res) => {
     res.status(404).send({ msg: "NOT FOUND THIS URL" })
